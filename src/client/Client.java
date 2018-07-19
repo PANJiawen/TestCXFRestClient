ebaf741011f5a96178a1c943a4dc5091f6779fb2
PANJiawen <pjwadsn2@gmail.com>
2018-07-19 21:24:30 +0200

    add a post function

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,6 +1,7 @@
 package client;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.client.WebClient;
 
@@ -10,9 +11,13 @@ public class Client {
 		
 		String url = "http://localhost:8080/rest/";
 		WebClient client = WebClient.create(url);
+		String fackXML = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
 		
-		System.out.println(client.path("/test/hello").accept(MediaType.TEXT_PLAIN).get(String.class));
+		
+		//System.out.println(client.path("/test/hello").accept(MediaType.TEXT_PLAIN).get(String.class));
 		System.out.println(client.path("/test/send/{input}","aaaa").accept(MediaType.TEXT_PLAIN).get(String.class));
+		client.replacePath(url);
+		System.out.println(client.path("/test/postxml/{input}",fackXML).post(fackXML).toString());
 	}
 	
 	
